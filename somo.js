00aabac6d7e4eb9d9a3ac1dabb8ca6e0ddc8342f
anthonroythomas <55889219+anthonroythomas@users.noreply.github.com>
2021-04-12 17:31:46 +0100

    Update somo.js

--- somo.js
+++ somo.js
@@ -4,9 +4,14 @@ describe('Somo test suite', function() {
 
     it('integration test', function(){
 
+        //Change the layout to iphone 6
         cy.viewport(550, 750) 
         cy.viewport('iphone-6') 
+        
+        //Navigate to the Somo website
         cy.visit("https://develop.somo.us")
+        
+        //Get the banner element and assert that it contains the text required
         cy.get('.css-p6jdiq > .chakra-heading').contains("Let us know you agree to cookies")
 
 
